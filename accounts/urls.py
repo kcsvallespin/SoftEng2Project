c4cdebcd1af37c6803de841db6a45a463cf2c04e
Kirby <kcsvallespin@addu.edu.ph>
2025-11-14 02:47:47 +0800

    Force commit all local changes

--- accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import load_users, delete_user
-
-urlpatterns = [
-    path('', load_users, name='load_users'),
-    path('delete/<int:user_id>/', delete_user, name='delete_user'),
-]
--- menu/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import create_item, view_items, edit_item
-
-urlpatterns = [
-    path('create/', create_item, name='create_item'),
-    path('view/', view_items, name='view_items'),
-    path('edit/<int:item_id>/', edit_item, name='edit_item'),
-    
-]
